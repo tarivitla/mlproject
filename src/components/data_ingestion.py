e2ae1fcf7125b2cd0e5414b784c3b2be0537dc13
“Ratnakartarivitla” <tarivitlaratnakar@gmail.com>
2024-07-16 02:58:26 +0530

    Deployment configuration

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,9 +14,9 @@ from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('arifacts',"train.csv")
-    test_data_path: str=os.path.join('arifacts',"test.csv")
-    raw_data_path: str=os.path.join('arifacts',"data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import pickle
 import numpy as np
 import pandas as pd
 import dill
@@ -32,7 +32,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            
+
             # model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -46,3 +46,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
